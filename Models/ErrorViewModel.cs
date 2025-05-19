37c3d695d274e8c4af565436075edd5939cfc755
KlimaJaroslaw <jaroslaw.klima@aprosoft.pl>
2025-05-19 08:41:41 +0200

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RottenPotatoes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
