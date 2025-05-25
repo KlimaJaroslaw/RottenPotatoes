47270747e15306bee05c4135852033f98e04d89e
KlimaJaroslaw <jaroslaw.klima@aprosoft.pl>
2025-05-25 11:51:45 +0200

    Modele

---
+++ Models/Permission.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RottenPotatoes.Models
+{
+    public class Permission
+    {
+        [Key]
+        public int Permission_ID { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RottenPotatoes.Models
+{
+    public class User
+    {
+        [Key]
+        public int User_ID { get; set; }
+        public string Login_Hash { get; set; }
+        public string Password_Hash { get; set; }
+
+        [ForeignKey(nameof(Permission))]
+        public int Permission_ID { get; set; }
+        public Permission? Permission { get; set; }
+    }
+}
