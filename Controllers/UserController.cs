79489fa6b91166b80b205f9c298146f428433841
KlimaJaroslaw <jaroslaw.klima@aprosoft.pl>
2025-05-26 08:45:38 +0200

    movie

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -16,11 +16,45 @@ namespace RottenPotatoes.Controllers
             _context = context;
             _session = session;
         }
-        #endregion       
+        #endregion
 
+        #region Http
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login([Bind("login_Hash,Password_Hash")] User user)
+        {
+            User u = ValidateUser(user);
+            if (u == null)
+            {
+                return View();
+            }
+            else
+            {
+                _session.Set("user", u);
+                return RedirectToAction("Index", "Movie");
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            _session.Remove("user");
+            return RedirectToAction("Login");
+        }
+        #endregion
+
+        #region Methods
+        private User ValidateUser(User user)
+        {
+            //jesli user istnieje w bazie danych -> zwracamy user, jesli nie -> null
+            user.User_ID = -1;
+            user.Permission_ID = -1;
+            user.Permission = new Permission() { Description="admin", Permission_ID = -1 };
+            return user;
+        }
+        #endregion
     }
 }
--- Models/Watchlist.cs
+++ Models/Watchlist.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using RottenPotatoes.Models;
 
 public class Watchlist
 {
