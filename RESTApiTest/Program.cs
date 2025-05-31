92cd369b6c92849aa8a2bebb33fc5860da02fb3d
pknot212121 <pknot@student.agh.edu.pl>
2025-05-31 18:48:50 +0200

    podstawowy get

--- RESTApiTest/Program.cs
+++ RESTApiTest/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text.Json;
+
+string username = "asdf";
+string authToken = "ceQ78t4rBXH421mHoLIhFqz5vnTfFnFaU0LK24pEcUk=";
+
+var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:5177/api/Api/Movies");
+httpWebRequest.ContentType = "application/json";
+httpWebRequest.Method = "GET";
+
+httpWebRequest.Headers.Add("X-Username", username);
+httpWebRequest.Headers.Add("X-Auth-Token", authToken);
+
+var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+{
+    var result = streamReader.ReadToEnd();
+    Console.WriteLine(result);
+}
